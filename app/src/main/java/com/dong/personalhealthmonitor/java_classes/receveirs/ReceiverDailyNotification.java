7fc00d9f1d73b1b7db078de8cf83b5421c66941e
Marco <viaimperiale@gmail.com>
2021-01-04 11:38:35 +0100

    bugfix: certe volte la notifica arriva nonostante l'utente abbia già salvato il report del giorno

--- app/src/main/java/com/dong/personalhealthmonitor/java_classes/receveirs/ReceiverDailyNotification.java
+++ app/src/main/java/com/dong/personalhealthmonitor/java_classes/receveirs/ReceiverDailyNotification.java
@@ -3,8 +3,10 @@ package com.dong.personalhealthmonitor.java_classes.receveirs;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.dong.personalhealthmonitor.java_classes.SQLite.ReportDB;
+import com.dong.personalhealthmonitor.java_classes.generic.CalendarManager;
 import com.dong.personalhealthmonitor.java_classes.generic.Report;
 import com.dong.personalhealthmonitor.java_classes.notifications.Notification;
 import com.dong.personalhealthmonitor.java_classes.shared_preferences.SettingsOperations;
@@ -17,7 +19,8 @@ public class ReceiverDailyNotification extends BroadcastReceiver {
         ReportDB reportDB = new ReportDB(context);
         SettingsOperations prefs = new SettingsOperations(context);
         reportDB.db.open();
-        List<Report> reports = reportDB.getReportsDay(prefs.getSelectedDayMillis());
+        List<Report> reports = reportDB.getReportsDay(new CalendarManager().getTodayMillis());
+        for (Report report : reports) Log.i("TAG", "" + report);
 
         if(reports.size() == 0) {
             String totalEntries = reports.size() + " entries";
--- app/src/main/java/com/dong/personalhealthmonitor/java_classes/receveirs/ReceiverPostponedNotification.java
+++ app/src/main/java/com/dong/personalhealthmonitor/java_classes/receveirs/ReceiverPostponedNotification.java
@@ -3,8 +3,10 @@ package com.dong.personalhealthmonitor.java_classes.receveirs;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.dong.personalhealthmonitor.java_classes.SQLite.ReportDB;
+import com.dong.personalhealthmonitor.java_classes.generic.CalendarManager;
 import com.dong.personalhealthmonitor.java_classes.generic.Report;
 import com.dong.personalhealthmonitor.java_classes.notifications.Notification;
 import com.dong.personalhealthmonitor.java_classes.shared_preferences.SettingsOperations;
@@ -18,8 +20,8 @@ public class ReceiverPostponedNotification extends BroadcastReceiver {
         ReportDB reportDB = new ReportDB(context);
         SettingsOperations prefs = new SettingsOperations(context);
         reportDB.db.open();
-        List<Report> reports = reportDB.getReportsDay(prefs.getSelectedDayMillis());
-
+        List<Report> reports = reportDB.getReportsDay(new CalendarManager().getTodayMillis());
+        for (Report report : reports) Log.i("TAG", "" + report);
         if (reports.size() == 0) {
             Notification notification = new Notification(context);
             notification.create("Your postponed reminder", "Hi again " + prefs.getName() + ", have you already filled today's report?");
